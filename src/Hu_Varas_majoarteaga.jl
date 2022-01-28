b181ff515455d5807e8389bce624ab47ad28acff
majoarteaga <88642146+majoarteaga@users.noreply.github.com>
2022-01-28 02:29:17 +0100

    Add more text

--- src/Hu_Varas_majoarteaga.jl
+++ src/Hu_Varas_majoarteaga.jl
@@ -1,15 +1,16 @@
 """
     Hu and Varas (WP, 2021) A Theory of Zombie Lending: REPLICATION
     
-    This code replicates Figure 2 (Panel A and Panel B) of Hu and Varas (2021). 
-    The left panel shows the joint valuations of the entrepreneur and the bank,
-    the right one only shows those of the bank. In this example, tb = 1.2921 and tg = 2.1006.
+    This code replicates Figure 2 (Panel A and Panel B) of Hu and Varas (2021).
     
     In the figure, the red, blue, and green lines represent the value functions of the
-    informed-good, the uninformed, and the informed-bad types, respectively. 
+    informed-good, the uninformed, and the informed-bad types, respectively.
+    The left panel shows the joint valuations of the entrepreneur and the bank,
+    the right one only shows those of the bank.
     
     Hu_Varas_majoarteaga.plot1 : Panel A of Figure 2
     Hu_Varas_majoarteaga.plot2 : Panel B of Figure 2
+
 """
 
 
@@ -35,7 +36,7 @@ module Hu_Varas_majoarteaga
     # Pkg.add("Plots")
     # Pkg.add("PlutoUI")
 
-    using LinearAlgebra
+    	using LinearAlgebra
 	using Statistics
 	using ForwardDiff
 	using Zygote
