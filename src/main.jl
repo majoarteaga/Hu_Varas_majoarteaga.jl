687b0cf27263332ec346713a02256752d9224f03
majoarteaga <88642146+majoarteaga@users.noreply.github.com>
2022-01-28 02:01:05 +0100

    Create main.jl

---
+++ src/main.jl
@@ -0,0 +1,27 @@
+  
+using LinearAlgebra
+using Statistics
+using ForwardDiff
+using Zygote
+using Optim
+using JuMP
+using Ipopt
+using BlackBoxOptim
+using Roots
+using SymPy
+using LeastSquaresOptim
+using Optim: converged, maximum, maximizer, minimizer, iterations
+using Pkg
+using BoundaryValueDiffEq
+using OrdinaryDiffEq
+using Plots
+using PlutoUI
+using DifferentialEquations
+
+
+
+include("Hu_Varas_majoarteaga.jl")
+
+
+Hu_Varas_majoarteaga.plot1
+Hu_Varas_majoarteaga.plot2
